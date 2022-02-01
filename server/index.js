4d5a9f53616cb9d70519cf428931467dfe80fd42
sisackov <1123889+sisackov@users.noreply.github.com>
2022-02-01 12:08:50 +0200

    added averageFantasyScore

--- server/index.js
+++ server/index.js
@@ -11,11 +11,13 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port);
 });
 
-const rule = new schedule.RecurrenceRule();
-rule.hour = 10;
-rule.dayOfWeek = 2;
-rule.tz = 'Etc/UTC'; //this will execute the job at 10:00 UTC every Tuesday
-schedule.scheduleJob(rule, scrapePlayerData);
+// scrapePlayerStats();
+
+// const rule = new schedule.RecurrenceRule();
+// rule.hour = 10;
+// rule.dayOfWeek = 2;
+// rule.tz = 'Etc/UTC'; //this will execute the job at 10:00 UTC every Tuesday
+// schedule.scheduleJob(rule, scrapePlayerData);
 
 //runs at minute 10 every 3rd hour
 // schedule.scheduleJob('10 */3 * * *', scrapeTeamDefenseStats);
