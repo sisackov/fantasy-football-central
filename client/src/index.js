fac39be675c04de6d2c9994eeae0e9da6b711cda
sisackov <1123889+sisackov@users.noreply.github.com>
2022-02-07 14:05:31 +0200

    Search page done

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
 import App from './App';
 import reportWebVitals from './utils/reportWebVitals';
 
