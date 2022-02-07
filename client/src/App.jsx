34ea7bc5a7f71d96d8c6915bb420b37cc20daaae
sisackov <1123889+sisackov@users.noreply.github.com>
2022-02-07 14:07:56 +0200

    apiDocs start

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavbarComponent from './components/NavbarComponent';
+import ApiDocsPage from './pages/ApiDocsPage';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import SearchPage from './pages/SearchPage.jsx';
 import SignupPage from './pages/SignupPage';
 import {
+    PATH_API,
     PATH_HOME,
     PATH_LOGIN,
     PATH_SEARCH,
@@ -22,6 +24,7 @@ function App() {
                     <Route exact path={PATH_SIGNUP} component={SignupPage} />
                     {/* <Route exact path={RESET_PASSWORD_PATH} component={Reset} /> */}
                     <Route exact path={PATH_SEARCH} component={SearchPage} />
+                    <Route exact path={PATH_API} component={ApiDocsPage} />
                 </Switch>
             </Router>
         </>
---
+++ client/src/pages/ApiDocsPage.jsx
@@ -0,0 +1,5 @@
+function ApiDocsPage() {
+    return null;
+}
+
+export default ApiDocsPage;
