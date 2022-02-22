89e1b06ca140d7699aee55bdb7ffcce16c332340
sisackov <1123889+sisackov@users.noreply.github.com>
2022-02-22 23:40:38 +0200

    heroku production check v2

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -4,8 +4,8 @@ export const getBaseUrl = () => {
     let baseURL;
 
     if (nodeEnv === 'production') {
-        // baseURL = 'https://fantasy-football-central.herokuapp.com';
-        baseURL = `${window.location.protocol}//${window.location.hostname}`;
+        baseURL = 'https://fantasy-football-central.herokuapp.com';
+        // baseURL = `${window.location.protocol}//${window.location.hostname}`;
     } else {
         //if (nodeEnv === 'aws') {
         baseURL = `${window.location.protocol}//${window.location.hostname}:${serverPort}`;
@@ -14,5 +14,6 @@ export const getBaseUrl = () => {
     console.log('NODE_ENV -> ', nodeEnv);
     console.log('REACT_APP_SERVER_PORT -> ', serverPort);
     console.log('BASE_URL', baseURL);
+    console.log('BASE_URL_WINDOW', `${window.location.protocol}//${window.location.hostname}`);
     return baseURL;
 };
